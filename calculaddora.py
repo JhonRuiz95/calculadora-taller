3ce11d3b32e4be657f982bf5d458b8b6ffad4476
JhonRuiz95 <70541547+JhonRuiz95@users.noreply.github.com>
2020-09-01 14:31:27 -0500

    Update calculaddora.py

--- calculaddora.py
+++ calculaddora.py
@@ -1,17 +1,17 @@
 
-def Suma(x, y):
-    return x + y
+def Suma(a, b):
+    return a + b
 
-def Resta(x, y):
-    return x - y
-
-
-def Multiplicacion(x, y):
-    return x * y
+def Resta(a, b):
+    return a - b
 
 
-def Dividicion(x, y):
-    return x / y
+def Multiplicacion(a, b):
+    return a * b
+
+
+def Dividicion(a, b):
+    return a / b
 
 
 
@@ -27,16 +27,20 @@ while not salir:
 
 
     opcion = input("Ingresa el número de la operación que deseas. ")
-    num1 = int(input("Ingrese el primer numero"))
-    num2 = int(input("Ingrese el segundo numero"))
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
     if opcion == "1":
+        print("el resultado es: ")
         print(Suma(num1,num2))
     elif opcion == "2":
+        print("el resultado es: ")
         print(Resta(num1,num2))
     elif opcion == "3":
-        print( Multiplicar(num1,num2))
+        print("el resultado es: ")
+        print( Multiplicacion(num1,num2))
     elif opcion == "4":
-        print(Dividir(num1,num2))
+        print("el resultado es: ")
+        print(Dividicion(num1,num2))
     elif opcion == "5":
         salir = True
         
